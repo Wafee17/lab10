7bf5c2be20453e5a81837a34d5b886be97536613
Wafee17 <mohammadwafiq58@gmail.com>
2025-04-25 11:45:37 +0530

    first commit

---
+++ src/main/java/prgm10/LAB10/App.java
@@ -0,0 +1,13 @@
+package prgm10.LAB10;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
